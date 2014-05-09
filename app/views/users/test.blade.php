aec4b6fd16424b7eefa425d8d6f089ec3b606965
sponrad <sponrad@gmail.com>
2014-05-09 11:54:00 -0700

    colors on test edit page

--- app/views/users/test.blade.php
+++ app/views/users/test.blade.php
@@ -230,71 +230,92 @@
   {{ Form::open(array('url'=>URL::route('postTest', array($project->id, $project->name, $test->id )), 'class'=>'form-horizontal', 'role'=>'form')) }}	  
   {{ Form::hidden('action', 'edittest') }}
 
-  <div class="form-group">
-    <label for="felevation" class="col-sm-4 ">Elevation</label>
-    <label for="fdensity_wet" class="col-sm-4 col-sm-offset-2">Wet Density</label>
+  <style scope>
+   .numbergroup{
+     background: #F4A460;
+     padding: 10px;
+     border-radius: 5px;     
+   }
+   .notegroup{
+     background: #CAE1FF;
+     padding: 10px;
+     border-radius: 5px;     
+     margin-top: 5px;
+     margin-bottom: 5px;
+   }
+  </style>
 
-    <div class="col-sm-4">
-      {{ Form::text('felevation', $test->elevation, array('class'=>'form-control', 'placeholder'=>'elevation', 'id'=>'felevationInput')) }}
+  <div class="numbergroup">
+    <div class="form-group">
+      <label for="felevation" class="col-sm-4 ">Elevation</label>
+      <label for="fdensity_wet" class="col-sm-4 col-sm-offset-2">Wet Density</label>
+
+      <div class="col-sm-4">
+	{{ Form::text('felevation', $test->elevation, array('class'=>'form-control', 'placeholder'=>'elevation', 'id'=>'felevationInput')) }}
+      </div>
+      <div class="col-sm-4 col-sm-offset-2">
+	{{ Form::text('fdensity_wet', number_format($test->density_wet,1), array('class'=>'form-control', 'placeholder'=>'density_wet', 'id'=>'fdensity_wet')) }}
+      </div>
     </div>
-    <div class="col-sm-4 col-sm-offset-2">
-      {{ Form::text('fdensity_wet', number_format($test->density_wet,1), array('class'=>'form-control', 'placeholder'=>'density_wet', 'id'=>'fdensity_wet')) }}
+
+    <div class="form-group">
+      <label for="fdensity_dry" class="col-sm-4">Dry Density</label>
+      <label for="fpercent_moisture" class="col-sm-4 col-sm-offset-2">Moisture %</label>
+
+      <div class="col-sm-4">
+	{{ Form::text('fdensity_dry', number_format($test->density_dry,1), array('class'=>'form-control', 'placeholder'=>'density_dry', 'id'=>'fdensity_dry')) }}
+      </div>
+      <div class="col-sm-4 col-sm-offset-2">
+	{{ Form::text('fpercent_moisture', number_format($test->percent_moisture, 1), array('class'=>'form-control', 'placeholder'=>'percent_moisture', 'id'=>'fpercent_moisture')) }}
+      </div>
     </div>
+
+    <div class="form-group">
+      <label for="fproctor" class="col-sm-4">Maximum Density</label>
+      <label for="fcompaction_percent" class="col-sm-4 col-sm-offset-2">Compaction %</label>
+
+      <div class="col-sm-4">
+	<select class="form-control" name="fproctor" id="fproctorInput">
+	  @foreach($proctors as $key => $proctor)
+	    <option id="{{ number_format($proctor->density_dry, 1) }}" value="{{$proctor->id}}">{{ number_format($proctor->density_dry, 1) }} @ {{ number_format($proctor->percent_moisture, 1) }}% - {{$proctor->name}}</option>
+	  @endforeach
+	</select>
+      </div>
+      <div class="col-sm-4 col-sm-offset-2">
+	{{ Form::text('fcompaction_percent', number_format($test->percent_compaction(), 1), array('class'=>'form-control', 'placeholder'=>'compaction_percent', 'id'=>'fcompaction_percent', 'disabled')) }}
+      </div>
+    </div>
+
   </div>
+  
+  <div class="notegroup">
 
-  <div class="form-group">
-    <label for="fdensity_dry" class="col-sm-4">Dry Density</label>
-    <label for="fpercent_moisture" class="col-sm-4 col-sm-offset-2">Moisture %</label>
+    <div class="form-group">
+      <label for="flocation" class="col-sm-3">Location</label>
+      <div class="col-sm-12">
+	{{ Form::textarea('flocation', $test->location, array('class'=>'form-control', 'placeholder'=>'location', 'size'=>'50x1')) }}
+      </div>
+    </div>
 
-    <div class="col-sm-4">
-      {{ Form::text('fdensity_dry', number_format($test->density_dry,1), array('class'=>'form-control', 'placeholder'=>'density_dry', 'id'=>'fdensity_dry')) }}
+    <div class="form-group">
+      <label for="fnotes" class="col-sm-3">Notes</label>
+      <div class="col-sm-12">
+	{{ Form::textarea('fnotes', $test->notes, array('class'=>'form-control', 'placeholder'=>'notes', 'size'=>'50x1')) }}
+      </div>
     </div>
-    <div class="col-sm-4 col-sm-offset-2">
-      {{ Form::text('fpercent_moisture', number_format($test->percent_moisture, 1), array('class'=>'form-control', 'placeholder'=>'percent_moisture', 'id'=>'fpercent_moisture')) }}
-    </div>
-  </div>
 
-  <div class="form-group">
-    <label for="fproctor" class="col-sm-4">Maximum Density</label>
-    <label for="fcompaction_percent" class="col-sm-4 col-sm-offset-2">Compaction %</label>
+    <div class="form-group">
+      <label for="fretest" class="col-sm-4">Retest of No:</label>
+      <label for="fdate" class="col-sm-4 col-sm-offset-2">Date:</label>
 
-    <div class="col-sm-4">
-      <select class="form-control" name="fproctor" id="fproctorInput">
-	@foreach($proctors as $key => $proctor)
-	  <option id="{{ number_format($proctor->density_dry, 1) }}" value="{{$proctor->id}}">{{ number_format($proctor->density_dry, 1) }} @ {{ number_format($proctor->percent_moisture, 1) }}% - {{$proctor->name}}</option>
-	@endforeach
-      </select>
+      <div class="col-sm-4">
+	{{ Form::text('fretest', $test->retest_of_number ? $test->retest_of_number : "", array('class'=>'form-control', 'placeholder'=>'retest', 'id'=>'fretest')) }}
+      </div>
+      <div class="col-sm-4 col-sm-offset-2">
+	{{ Form::text('fdate', $test->created_at->format('m/d/Y H:i'), array('class'=>'form-control', 'placeholder'=>'date', 'id'=>'fdate')) }}
+      </div>
     </div>
-    <div class="col-sm-4 col-sm-offset-2">
-      {{ Form::text('fcompaction_percent', number_format($test->percent_compaction(), 1), array('class'=>'form-control', 'placeholder'=>'compaction_percent', 'id'=>'fcompaction_percent', 'disabled')) }}
-    </div>
-  </div>
-
-  <div class="form-group">
-    <label for="flocation" class="col-sm-3">Location</label>
-    <div class="col-sm-12">
-      {{ Form::textarea('flocation', $test->location, array('class'=>'form-control', 'placeholder'=>'location', 'size'=>'50x1')) }}
-    </div>
-  </div>
-
-  <div class="form-group">
-    <label for="fnotes" class="col-sm-3">Notes</label>
-    <div class="col-sm-12">
-      {{ Form::textarea('fnotes', $test->notes, array('class'=>'form-control', 'placeholder'=>'notes', 'size'=>'50x1')) }}
-    </div>
-  </div>
-
-  <div class="form-group">
-    <label for="fretest" class="col-sm-4">Retest of No:</label>
-    <label for="fdate" class="col-sm-4 col-sm-offset-2">Date:</label>
-
-    <div class="col-sm-4">
-      {{ Form::text('fretest', $test->retest_of_number ? $test->retest_of_number : "", array('class'=>'form-control', 'placeholder'=>'retest', 'id'=>'fretest')) }}
-    </div>
-    <div class="col-sm-4 col-sm-offset-2">
-      {{ Form::text('fdate', $test->created_at->format('m/d/Y H:i'), array('class'=>'form-control', 'placeholder'=>'date', 'id'=>'fdate')) }}
-    </div>
-  </div>
+  </div>  
 
   <div class="form-group">
     <div class="col-sm-offset-3 col-sm-9">
@@ -302,6 +323,6 @@
       <a class="btn btn-default" href="/home/{{ $project->id}}-{{$project->name}}/tests#{{ $test->id }}">Cancel</a>
     </div>
   </div>
-  
+
   {{ Form::close() }}
 @stop
