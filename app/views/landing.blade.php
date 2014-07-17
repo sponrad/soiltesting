5fe49a71db597d85fbe26ed8f24ff69d1c4ab807
sponrad <sponrad@gmail.com>
2014-07-16 21:49:18 -0700

    images to landing page

--- app/views/landing.blade.php
+++ app/views/landing.blade.php
@@ -6,9 +6,65 @@
 
 @section('content') 
   <div class="row">
-    <div class="col-md-6 col-md-offset-3">
+    <div class="col-md-8 col-md-offset-2">
       <h1 class="cover-heading">Density Test Software</h1>
       <p class="lead">Digitize density tests in the field and store them in the cloud</p>
+     
+      <style>
+	.carousel-caption{
+	 text-stroke: 1px black;
+	 color: white;
+	 text-shadow:    
+	 -1px -1px .1em #000,  
+	 1px -1px 0.1em #000,
+	 -1px 1px 0.1em #000,
+	 1px 1px 0.1em #000;
+       }
+      </style>
+
+      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" style="height: 350px;">
+	<!-- Wrapper for slides -->
+	<div class="carousel-inner">
+	  <div class="item active">
+	    <img src="/images/dp-home.png" alt="Project Selection">
+	    <div class="carousel-caption">
+	      Supports Multiple Projects
+	    </div>
+	  </div>
+
+	  <div class="item">
+	    <img src="/images/dp-project.png" alt="Project Overview">
+	    <div class="carousel-caption">
+	      Project Overview
+	    </div>
+	  </div>
+
+	  <div class="item">
+	    <img src="/images/dp-tests.png" alt="Test Table">
+	    <div class="carousel-caption">
+	      Test Listing Page
+	    </div>
+	  </div>
+
+	  <div class="item">
+	    <img src="/images/dp-taketest.png" alt="Record a Density Test">
+	    <div class="carousel-caption">
+	      Record a Test From any Screen
+	    </div>
+	  </div>
+	</div>
+
+	<!-- Controls -->
+	<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+	  <span class="glyphicon glyphicon-chevron-left"></span>
+	</a>
+	<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+	  <span class="glyphicon glyphicon-chevron-right"></span>
+	</a>
+      </div>
+
+      <br>
+
       <p class="lead" style="text-align: center;">
 	@if(!Auth::check())
 	  <a class="btn btn-primary btn-lg" href="/register">Register</a>
